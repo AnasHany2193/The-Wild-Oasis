d23dac00b730f6f68c8cdb9847eb186242a756db
Anas Hany <anashany219@gmail.com>
2024-09-12 18:29:39 +0300

    Add global styles with styled-components 🎨

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 2rem;
+  font-weight: 600;
+`;
+
 function App() {
-  return <div>Hello, Wild Oasis</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <H1>Hello, Wild Oasis</H1>
+    </>
+  );
 }
 
 export default App;
