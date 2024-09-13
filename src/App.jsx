5651b1d9e509919cd013c3602adedda005bd7411
Anas Hany <anashany219@gmail.com>
2024-09-13 18:09:51 +0300

    Build the main app layout structure 🏗️

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import Users from "./pages/Users";
 import Cabins from "./pages/Cabins";
 import Account from "./pages/Account";
+import AppLayout from "./ui/AppLayout";
 import Bookings from "./pages/Bookings";
 import Settings from "./pages/Settings";
 import Dashboard from "./pages/Dashboard";
@@ -17,15 +18,18 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route element={<AppLayout />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route index element={<Navigate replace to="dashboard" />} />
+
+            <Route path="users" element={<Users />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="account" element={<Account />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
 
           <Route path="login" element={<Login />} />
-          <Route path="users" element={<Users />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="account" element={<Account />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="settings" element={<Settings />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
