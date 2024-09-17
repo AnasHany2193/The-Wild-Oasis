42958149f246cd46bd08572353d804c7cdf6af86
Anas Hany <anashany219@gmail.com>
2024-09-17 23:20:10 +0300

    Add client-side filtering for cabins to improve search functionality 🔍

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,32 +1,27 @@
-import styled from "styled-components";
+import { useSearchParams } from "react-router-dom";
 
 import CabinRow from "./CabinRow";
-import Spinner from "../../ui/Spinner";
-import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import Spinner from "../../ui/Spinner";
+import { useCabins } from "./useCabins";
 
 function CabinTable() {
+  const [searchParams] = useSearchParams();
   const { cabins, isLoading } = useCabins();
 
-  return isLoading ? (
-    <Spinner />
-  ) : (
+  if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+  filterValue === "no-discount"
+    ? (filteredCabins = cabins.filter((cabin) => cabin.discount === 0))
+    : filterValue === "with-discount"
+    ? (filteredCabins = cabins.filter((cabin) => cabin.discount > 0))
+    : (filteredCabins = cabins);
+
+  return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
         <Table.Header>
@@ -39,7 +34,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
