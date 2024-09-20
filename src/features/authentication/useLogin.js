7e4ffb2304e1b2771045f4d52e778bb6a55dcb92
Anas Hany <anashany219@gmail.com>
2024-09-20 22:41:15 +0300

    Fix bug: use setQueryData instead of setQueriesData 🔧🐛

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -12,7 +12,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       navigate("/dashboard", { replace: true });
-      queryClient.setQueriesData(["user", user]);
+      queryClient.setQueryData(["user", user.user]);
     },
     onError: (err) => toast.error("Provided email or password are incorrect"),
   });
