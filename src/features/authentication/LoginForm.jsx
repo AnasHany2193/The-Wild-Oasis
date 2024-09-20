429571414a7adcab7c222e544880f1df135501cc
Anas Hany <anashany219@gmail.com>
2024-09-20 21:46:56 +0300

    Implement user login authentication with Supabase 🔐

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,38 +1,53 @@
 import { useState } from "react";
-import Button from "../../ui/Button";
+
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
+import Button from "../../ui/Button";
 import FormRowVertical from "../../ui/FormRowVertical";
+import { useLogin } from "./useLogin";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { login, isLoading } = useLogin();
+  const [email, setEmail] = useState("anas@example.com");
+  const [password, setPassword] = useState("123456");
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!email || !password) return;
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
       <FormRowVertical label="Email address">
         <Input
-          type="email"
           id="email"
+          type="email"
+          value={email}
+          disabled={isLoading}
           // This makes this form better for password managers
           autoComplete="username"
-          value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
       </FormRowVertical>
+
       <FormRowVertical label="Password">
         <Input
-          type="password"
           id="password"
-          autoComplete="current-password"
+          type="password"
           value={password}
+          disabled={isLoading}
+          autoComplete="current-password"
           onChange={(e) => setPassword(e.target.value)}
         />
       </FormRowVertical>
+
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoading}>
+          {!isLoading ? "Login" : <SpinnerMini />}
+        </Button>
       </FormRowVertical>
     </Form>
   );
