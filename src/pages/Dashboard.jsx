5638c6fc99e7ea53dc3860598baba925aed2d0fd
Anas Hany <anashany219@gmail.com>
2024-09-22 14:56:01 +0300

    Compute recent bookings and stays for dashboard analytics 📊

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,8 +2,16 @@ import Row from "../ui/Row";
 import Heading from "../ui/Heading";
 import DashboardLayout from "../features/dashboard/DashboardLayout";
 import DashboardFilter from "../features/dashboard/DashboardFilter";
+import { useRecentBookings } from "../features/dashboard/useRecentBookings";
+import Spinner from "../ui/Spinner";
+import { useRecentStays } from "../features/dashboard/useRecentStays";
 
 function Dashboard() {
+  const { bookings, isLoading: isBookingsLoading } = useRecentBookings();
+  const { stays, isLoading: isStaysLoading, confirmedStays } = useRecentStays();
+
+  if (isBookingsLoading || isStaysLoading) return <Spinner />;
+
   return (
     <>
       <Row type="horizontal">
